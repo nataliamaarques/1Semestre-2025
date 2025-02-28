90d987e23ce24839b90f1d1c4572853392d2182e
nataliamaarques <natalia.vmarques123@gmail.com>
2025-02-27 22:22:51 -0300

    Exercícios (1 e 2) feitos em aula 27-02

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -6,33 +6,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController   {
-/*@GetMapping("")
 
-    public String HelloWorld1(){
-        return "Hello";
-    }*/
-     
-   /*resolução do professor
-   @GetMapping("/get-idade/{idade}")
-   public String retornaIdade(Integer idade){
-        try{
-            if(idade < 0){
-                throw new NumberFormatException()
-            };
-            if(idade < 12) {
-                return "Criança";
-            }else if(idade <= 18){
-                return "Adolescente";
-            }else if(idade <= 60){
-                return "Adulto";
+    @GetMapping("/ex1/{numero}")
+    public String numeroparimpar(@PathVariable int numero){
+        if (numero>=0) {
+            if (numero % 2 == 0){
+                return "par";
             }else{
-                return "Idoso";
+                return "ímpar";  
+            }  
+        } else{
+            return "Valor inválido";
+           
+        }
+    }
+
+    @GetMapping("/ex2/{peso}/{altura}")
+    public String calculadora (@PathVariable double peso, @PathVariable double altura){
+        double imc;
+        imc = peso / (altura*altura);
+        if(imc >=0 ){
+            if(imc<=18.5){
+                return "abaixo do peso";
+            }else if(imc>18.5 &&  imc<=24.9){
+                return "peso normal";
+            }else if(imc>=25 && imc <=29.9){
+                return "sobrepeso";
+            }else if(imc>=30 && imc <= 34.9){
+                return "obesidade grau 1";
+            }else if(imc>=35.0 && imc <=39.0){
+            return "obesidade grau 2";
+            }else{
+                return "obesidade grau 3";
             }
-        } catch (NumberFormatException e){
-            return "Idade inválida";
-        }*/
+        }else{
+            return "valor inválido";
+        }
+       
+    }
     
-    @GetMapping("{idade}")
+    @GetMapping("/ex5/{idade}")
     public String classificaIdade(@PathVariable int idade){
 
         if (idade > 0 && idade < 12){
@@ -47,17 +60,5 @@ public class ExercicioController   {
             return "Idade inválida";
         }
 
-    }
-/*@GetMapping("{nome}")
-
-    public String HelloWorld(@PathVariable String nome){
-        return nome;
-    }*/
-
-    
-/*@GetMapping("{nome}")
-    public String HelloWorld(@PathVariable Optional<String> nome){
-        return nome.isPresent() ? nome.get() : "dd";
-    }*/
-    
+    } 
 }
