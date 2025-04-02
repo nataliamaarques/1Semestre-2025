afa0012c7d9cd33c14b8e362f35cfae3ceb61b02
nataliamaarques <natalia.vmarques123@gmail.com>
2025-04-02 19:56:05 -0300

    códigos atualizados

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# 1Semestre-2025
+git clone https://github.com/h2danilofatec/projeto-1semestre-2025.git
 
-git clone https://github.com/nataliamaarques/1Semestre-2025.git
-git config --global user.name "nataliamaarques" 
+git config –global user.name “h2danilofatec”
+
+git config –global user.email “danilo.valim01@fatecitapetininga.edu.br”
--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -1,64 +1,48 @@
 package com.fatec.projeto.projeto2025.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
 @RestController
-public class ExercicioController   {
+public class ExercicioController {
 
-    @GetMapping("/ex1/{numero}")
-    public String numeroparimpar(@PathVariable int numero){
-        if (numero>=0) {
-            if (numero % 2 == 0){
-                return "par";
-            }else{
-                return "ímpar";  
-            }  
-        } else{
-            return "Valor inválido";
-           
-        }
+    @GetMapping("")
+    public String HelloWorld1() {
+        return "hello6";
     }
 
-    @GetMapping("/ex2/{peso}/{altura}")
-    public String calculadora (@PathVariable double peso, @PathVariable double altura){
-        double imc;
-        imc = peso / (altura*altura);
-        if(imc >=0 ){
-            if(imc<=18.5){
-                return "abaixo do peso";
-            }else if(imc>18.5 &&  imc<=24.9){
-                return "peso normal";
-            }else if(imc>=25 && imc <=29.9){
-                return "sobrepeso";
-            }else if(imc>=30 && imc <= 34.9){
-                return "obesidade grau 1";
-            }else if(imc>=35.0 && imc <=39.0){
-            return "obesidade grau 2";
-            }else{
-                return "obesidade grau 3";
-            }
-        }else{
-            return "valor inválido";
-        }
-       
+    @GetMapping("{nome}")
+    public String HelloWorld(@PathVariable Optional<String> nome) {
+        return nome.isPresent() ? nome.get() : "dd";
     }
-    
-    @GetMapping("/ex5/{idade}")
-    public String classificaIdade(@PathVariable int idade){
+   
+    @GetMapping("/get-idade/{idade}")
+    public String RetornaIdade(@PathVariable Integer idade) {
+        try {
+         if (idade < 0) {
+            throw new NumberFormatException();
+         }
 
-        if (idade > 0 && idade < 12){
-            return "Criança";
-        }else if(idade >= 12 && idade <=18){
+         if (idade < 12 ) {
+            return "Crianca";
+         } else if (idade <= 18) {
             return "Adolescente";
-        }else if(idade >= 19 && idade <= 60){
+         } else if (idade <= 60) {
             return "Adulto";
-        }else if(idade > 60){
+         } else {
             return "Idoso";
-        }else{
-            return "Idade inválida";
+         }
+            
+        } catch (NumberFormatException e) {
+            return "idade invalida";
         }
-
-    } 
+    }
 }
